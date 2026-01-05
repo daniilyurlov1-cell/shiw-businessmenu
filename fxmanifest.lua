bc13a98e876b50ae3c8607fa0074550bda9a5672
Shadowshiw <daniilyurlov1@gmail.com>
2026-01-05 16:50:45 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,10 @@ fx_version 'cerulean'
 games { 'rdr3' }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-name 'rsg-businessmenu'
+name 'shiw-businessmenu'
 description 'Business Menu System for RSG Framework'
-author 'Developer'
-version '1.0.0'
+author 'Shiw Magic Scripts'
+version '3.0.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
@@ -20,5 +20,6 @@ server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/main.lua'
 }
+
 
 lua54 'yes'
